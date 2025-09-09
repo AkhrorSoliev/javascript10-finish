5701a0c72b6197cca84fa70d6812b3a4da673652
akhrorsoliev <akhrorweb@gmail.com>
2025-09-09 20:54:04 +0500

    add replies content and make request for comments

--- interactive-comments-section/js/app.js
+++ interactive-comments-section/js/app.js
@@ -1,9 +1,49 @@
 import { axiosInstance } from "./request.js";
 const template = document.getElementById("comment-card");
 const commentCards = document.querySelector(".comment-cards");
+const commentReplies = document.getElementById("comment-replies");
 
-const rendeReplies = (replies) => {
-  console.log(replies);
+let currentUser;
+
+const rendeReplies = (replies, container) => {
+  replies.forEach((reply) => {
+    const { id, content, createdAt, score, user, replyingTo } = reply;
+    const clone = commentReplies.content.cloneNode(true);
+
+    // select elements from template
+    const pictureEl = clone.querySelector("picture");
+    const sourceWebp = pictureEl.querySelector('source[type="image/webp"]');
+    const imgEl = pictureEl.querySelector("img.comment-card__avatar");
+
+    const card__username = clone.querySelector(".comment-card__username");
+    const card__time = clone.querySelector(".comment-card__time");
+    const card__body = clone.querySelector(".comment-card__body");
+    const card__amount = clone.querySelector(".amount");
+    const card__owner = clone.querySelector(".comment-card__owner");
+    console.log(card__owner);
+    if (currentUser.username == user.username) {
+      card__owner.style.display = "block";
+    }
+
+    // give data to seleted elements
+    card__username.textContent = user.username;
+    card__time.textContent = createdAt;
+    card__body.innerHTML = `<a href="#" class="comment-card__replyTo"> @${replyingTo}</a> ${content}`;
+    card__amount.textContent = score;
+
+    if (user?.image?.webp && sourceWebp) {
+      sourceWebp.srcset = user.image.webp;
+    }
+    if (user?.image?.png && imgEl) {
+      imgEl.src = user.image.png;
+    }
+    if (imgEl) {
+      imgEl.alt = `${user?.username ?? "User"} avatar`;
+    }
+
+    // rendering cart to ui
+    container.appendChild(clone);
+  });
 };
 
 const renderComment = (comments) => {
@@ -40,7 +80,8 @@ const renderComment = (comments) => {
 
     // replies rendering
     if (replies.length > 0) {
-      rendeReplies(replies);
+      const commentReplies = clone.querySelector(".comment-replies");
+      rendeReplies(replies, commentReplies);
     }
 
     // rendering cart to ui
@@ -50,8 +91,11 @@ const renderComment = (comments) => {
 
 document.addEventListener("DOMContentLoaded", async () => {
   try {
-    const res = await axiosInstance("/comments");
-    renderComment(res.data);
+    const resCurrentUser = await axiosInstance("/currentUser");
+    currentUser = resCurrentUser.data;
+
+    const resComments = await axiosInstance("/comments");
+    renderComment(resComments.data);
   } catch (error) {
     console.log(error.message);
   } finally {
